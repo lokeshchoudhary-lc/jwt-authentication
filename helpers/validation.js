0ca914cf73945014df39a9257fedda87491b4615
Lokesh Choudhary <lokeshchoudhary8949@gmail.com>
2021-08-17 17:22:29 +0530

    feat: Add validation

---
+++ helpers/validation.js
@@ -0,0 +1,10 @@
+const joi = require('joi');
+
+const authSchema = joi.object({
+  email: joi.string().email().lowercase().required(),
+  password: joi.string().min(2).required(),
+});
+
+module.exports = {
+  authSchema,
+};
