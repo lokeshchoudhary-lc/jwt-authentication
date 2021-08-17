ee627124c135d88b22c2c030af174cfc63e63157
Lokesh Choudhary <lokeshchoudhary8949@gmail.com>
2021-08-17 17:22:43 +0530

    feat: Add schema

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const bcrypt = require('bcrypt');
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+UserSchema.pre('save', async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(8);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+UserSchema.methods.isValidPassword = async function (password) {
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (error) {
+    throw error;
+  }
+};
+
+const User = mongoose.model('user', UserSchema);
+
+module.exports = User;
